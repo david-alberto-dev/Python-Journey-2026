c1bc3f62919b5d35b4aecba7d6f20ef8c87e2a73
David Alberto <cars.renter351@passinbox.com>
2026-04-15 21:48:34 +0300

    feat: add ToDo CLI and organize project structure

---
+++ 03-To-Do-CLI/to_do_app.py
@@ -0,0 +1,60 @@
+"""
+Miniproject #3: To Do App
+"""
+
+tasks = []
+
+def show_tasks():
+    if not tasks:
+        print("There are no tasks.")
+    else:
+        for index, task in enumerate(tasks):
+            print(f"{index + 1}. {task}")
+
+while True:
+    print("""
+    --- To Do App ---
+    1. View tasks
+    2. Add new task
+    3. Delete task
+    4. Quit program
+          """)
+    option = input("Select an option (1-4): ")
+    match option:
+        case "1":
+            show_tasks()
+            continue
+        case "2":
+            new_task = input("Enter new task: ")
+            tasks.append(new_task)
+            print(f"New task added: {new_task}")
+            continue
+        case "3":
+            show_tasks()
+            if len(tasks) == 0:
+                print("There are no tasks, so you can't delete any.")
+                continue
+            else:
+                try:
+                    deleted_task_index = int(input("Enter task index to delete: "))
+                    real_index = deleted_task_index - 1
+                    if deleted_task_index < 0 or deleted_task_index >= len(tasks):
+                        print("Invalid index. Try again.")
+                        continue
+                    else:
+                        deleted_task = tasks.pop(real_index)
+                        print(f"Task deleted: {deleted_task}")
+                        continue
+                except ValueError:
+                    print("Invalid option. Try to enter a number from 1 to 4.")
+                    continue
+        case "4":
+            quit_confirmation = input("Are you sure you want to quit? (y/n): ")
+            if quit_confirmation == "y":
+                print("Quitting...")
+                break
+            else:
+                continue
+        case _:
+            print("Invalid option. Try to enter a number from 1 to 4.")
+            continue
